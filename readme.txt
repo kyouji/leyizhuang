3acaa3377412765233aac9fcbf3aba60db8f6a99
guozhy <guozhy>
2015-01-29 10:28:26 +0800

    首页跳转

---
+++ readme.txt
@@ -0,0 +1,15 @@
+架构:SpringMvc+MYSQL5.0+freemark
+包命名：
+src/main/java:核心业务处理
+controller:--业务处理
+		back:代表后台处理相关：
+		front：前台业务处理相关;
+		
+src/main/resources：所有页面相关，
+		static:js/css/images。。。存放目录
+		templates:所有页面存放目录
+			back:代表后台页面相关：
+			front：前台页面处理相关;
+			
+			
+														----解说人---:郭正洋
--- src/main/java/com/ynyes/rongcheng/controller/front/TypelistController.java
+++ src/main/java/com/ynyes/rongcheng/controller/front/TypelistController.java
@@ -4,31 +4,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 前端首页控制
+ * 商品类型集合
  * 
- * IndexController<BR>
+ * TypelistController<BR>
  * 创建人:guozhengyang <BR>
- * 时间：2015年1月29日-上午9:45:13 <BR>
+ * 时间：2015年1月29日-上午10:10:58 <BR>
  * @version 1.0.0
  *
  */
 @Controller
-@RequestMapping("/")
-public class IndexController {
+@RequestMapping("/type")
+public class TypelistController {
     /**
      * 
-     * 前台首页跳转<BR>
+     * 跳转到商品类型列表页面<BR>
      * 方法名：index<BR>
      * 创建人：guozhengyang <BR>
-     * 时间：2015年1月29日-上午9:50:50 <BR>
+     * 时间：2015年1月29日-上午10:19:44 <BR>
      * @return String<BR>
      * @param  [参数1]   [参数1说明]
      * @param  [参数2]   [参数2说明]
      * @exception <BR>
      * @since  1.0.0
      */
-    @RequestMapping
+    @RequestMapping("/list")
     public String index(){
-        return "/front/index";
+        return "/front/type_list";
     }
 }
