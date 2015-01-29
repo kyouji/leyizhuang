537613e16f6bfe7298490c2e97778fc6a3775a0a
guozhy <guozhy>
2015-01-29 09:48:11 +0800

    controller

---
+++ src/main/java/com/ynyes/rongcheng/controller/front/IndexController.java
@@ -0,0 +1,17 @@
+package com.ynyes.rongcheng.controller.front;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 前端首页控制
+ * 
+ * IndexController<BR>
+ * 创建人:guozhengyang <BR>
+ * 时间：2015年1月29日-上午9:45:13 <BR>
+ * @version 1.0.0
+ *
+ */
+@Controller
+public class IndexController {
+
+}
