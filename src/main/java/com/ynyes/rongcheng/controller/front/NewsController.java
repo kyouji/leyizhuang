df1fac01ad017afeb46160c2bbdb70c1030947ec
guozhy <guozhy>
2015-03-06 18:10:49 +0800

    controller

--- src/main/java/com/ynyes/rongcheng/controller/front/NewsController.java
+++ src/main/java/com/ynyes/rongcheng/controller/front/NewsController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
